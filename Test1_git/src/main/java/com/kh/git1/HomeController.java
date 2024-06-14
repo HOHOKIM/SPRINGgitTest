b0aa3b3066c634704e64a045dcaa6855952374ad
user1 <user1@DESKTOP-BIRC19S>
2024-06-14 10:50:47 +0900

    하나 넣었지

--- Test1_git/src/main/java/com/kh/git1/HomeController.java
+++ Test1_git/src/main/java/com/kh/git1/HomeController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
-	
+	//내용 바뀜
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
